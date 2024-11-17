29627be4008a7631f9d0e9f43b3a154cdbde62d3
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-18 00:44:50 +0530

    Update interface.java

--- interface.java
+++ interface.java
@@ -2,13 +2,18 @@
 interface bike
   {
     void applyBrake(int brake);
-    void useAccelerator();
+    void useAccelerator(int accelerate);
     void changeGears();
   }
 class apacheBike implements bike
   {
-    void applyBrake()
+    void applyBrake(int brake)
     {
       latestSpeed=latestSpeed - brake;
     }
+    void useAccelerator(int accelerate)
+    {
+      latestSpeed=latestSpeed + accelerate;
+    }
+    
     
