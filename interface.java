a976bcaa9a377436e60855a2d9b4a8bfdd0adeed
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-18 01:07:51 +0530

    Update interface.java

--- interface.java
+++ interface.java
@@ -12,6 +12,13 @@ class apacheBike implements bike
     InputStreamReader in = new InputStreamReader(System.in);
     BufferedReader br = new BufferedReader(in);   //taking inout using inputstreamreader
     //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  //single line inout using Buffered Reader
+    System.out.println("Enter the current speed of the car and the current gear : ");
+    int latestSpeed =  Integer.parseInt(br.readline);
+    int currentGear = Integer.parseInt(br.readLine);
+    System.out.println(("Enter the accelaration decceleration and change in gear required : ");
+    int accelerate=Integer.parseInt(br.readLine);
+    int brake =  Integer.parseInt(br.readLine);
+    int currentGear = Integer.parseInt(br.readLine);
     void applyBrake(int brake)
     {
       latestSpeed=latestSpeed - brake;
