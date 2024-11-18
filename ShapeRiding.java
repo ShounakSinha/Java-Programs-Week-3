26e13ec524e02a76f1a98c3f551b62bc82f2023a
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-19 00:18:32 +0530

    Update ShapeRiding.java

--- ShapeRiding.java
+++ ShapeRiding.java
@@ -1,1 +1,24 @@
+import java.io.*;
+class Shape
+{
+    public void area()
+    {
+        System.out.println("Habi jabi");
+    }
+}
+class Rectangle extends Shape
+{
+    int length, breadth, ar;
+    
+    Rectangle(int length, int breadth)
+    {
+        this.length=length;
+        this.breadth=breadth;
+    }
+    public void area()
+    {
+        ar=length*breadth;
+        System.out.println("The area of a rectangle is : "+ar+" square units");
+    }
+}
 
