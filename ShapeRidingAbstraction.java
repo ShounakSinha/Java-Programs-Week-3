17b1668e4146da0afd8ae88baa2423d542fa6ff4
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-19 00:25:33 +0530

    Update ShapeRidingAbstraction.java

--- ShapeRidingAbstraction.java
+++ ShapeRidingAbstraction.java
@@ -33,3 +33,19 @@ class Circle extends Shape
     }
 }
 
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the length and breadth of the rectangle : ");
+        int length = Integer.parseInt(in.readLine());
+        int breadth = Integer.parseInt(in.readLine());
+        Shape rect=new Rectangle(length,breadth);
+        rect.area();
+        System.out.println("Enter the radius of the circle : ");
+        double radius = Double.parseDouble(in.readLine());
+        Shape circ= new Circle(radius);
+        circ.area();
+    }
+}
