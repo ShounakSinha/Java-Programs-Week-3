5923e492e0a7c3a6f523057d0b4506d9c20af756
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-19 00:24:37 +0530

    Update ShapeRidingAbstraction.java

--- ShapeRidingAbstraction.java
+++ ShapeRidingAbstraction.java
@@ -1,1 +1,35 @@
+import java.io.*;
+abstract class Shape
+{
+    abstract public void area();
+}
+class Rectangle extends Shape
+{
+    int length, breadth, ar;
+    
+    Rectangle(int length, int breadth)
+    {
+        this.length=length;
+        this.breadth=breadth;
+    }
+    public void area()
+    {
+        ar=length*breadth;
+        System.out.println("The area of a rectangle is : "+ar+" square units");
+    }
+}
+class Circle extends Shape
+{
+    double radius, ar;
+    
+    Circle(double radius)
+    {
+        this.radius=radius;
+    }
+    public void area()
+    {
+        ar=3.142*radius*radius;
+        System.out.println("The area of the circle is : "+ar+" square units");
+    }
+}
 
