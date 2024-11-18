fcc67fbca354b5394918e840cb78109bd4d05102
Shounak Sinha <126563210+ShounakSinha@users.noreply.github.com>
2024-11-19 00:19:40 +0530

    Update ShapeRiding.java

--- ShapeRiding.java
+++ ShapeRiding.java
@@ -35,4 +35,20 @@ class Circle extends Shape
         System.out.println("The area of the circle is : "+ar+" square units");
     }
 }
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the length and breadth of the rectangle : ");
+        int length = Integer.parseInt(in.readLine());
+        int breadth = Integer.parseInt(in.readLine());
+        Shape rect=new Rectangle(length,breadth);
+        rect.area();
+        System.out.println("Enter the radius of the circle : ");
+        double radius = Double.parseDouble(in.readLine());
+        Shape circ= new Circle(radius);
+        circ.area();
+    }
+}
 
